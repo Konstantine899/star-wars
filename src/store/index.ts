2d7f45422afb4a3780836969f5d23ba91ad1336a
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-09-09 12:15:50 +0300

    feat: install redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 import { rootReducer } from "./reducers/rootReducer";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export type TRootReducer = ReturnType<typeof rootReducer>;
